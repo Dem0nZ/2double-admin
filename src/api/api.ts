68a4641d58d55ee0288025cba5c5c4202c11143a
Dem0nZ <cbpskov@gmail.com>
2020-06-19 00:08:30 +0300

    create axios instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import {LoginResponse} from "../models";
 
+const instance = axios.create({
+    baseURL: 'https://c5e0f238-2039-4dda-b43d-8f213414bc2e.mock.pstmn.io'
+});
+
 export const authAPI = {
     login(login: string, password: string) {
-        return axios.post<LoginResponse>('https://c5e0f238-2039-4dda-b43d-8f213414bc2e.mock.pstmn.io/auth', {
+        return instance.post<LoginResponse>('/auth', {
             login,
             password
         });
