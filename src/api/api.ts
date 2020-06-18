aa9a9380fa212e4b750ccd764e1ede3c19b1efcb
Dem0nZ <cbpskov@gmail.com>
2020-06-19 00:30:43 +0300

    add interceptor reqest

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,18 @@
 import axios from 'axios';
 import {LoginResponse} from "../models";
+import store from "../store";
 
 const instance = axios.create({
     baseURL: 'https://c5e0f238-2039-4dda-b43d-8f213414bc2e.mock.pstmn.io'
+});
+
+// Add a request interceptor
+axios.interceptors.request.use( (config) => {
+    const token = store.getState().login.token;
+    if (token != undefined) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export const authAPI = {
